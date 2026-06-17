74ced4e01125384a1647b6a814c1b8ba13955b18
Dylan Goncalves <dylanphx93@gmail.com>
2026-06-17 22:25:35 +0200

    add task edit logic to controller and model

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -32,5 +32,26 @@ class TaskController extends Controller {
       $filtered = $this->taskModel->filterTasks('id', $id);
       $this->view->task = array_shift($filtered);
   }
-  
+
+  public function editAction() {
+  if ($this->getRequest()->isPost()) {
+          $task = [
+             "id" => $this->getRequest()->getParam('id'),
+             "title" => $this->getRequest()->getParam('title'),
+             "description" => $this->getRequest()->getParam('description'),
+             "user" => $this->getRequest()->getParam('user'),
+             "status" => $this->getRequest()->getParam('status'),
+             "start_time" => $this->getRequest()->getParam('start_time') ?: "",
+             "end_time" => $this->getRequest()->getParam('end_time') ?: ""
+          ];
+          $this->taskModel->editTask($task);
+          header("Location: /phpInitialDemo/web/task/index");
+          exit;
+      } else {
+          $id = $this->_getParam('id');
+          $filtered = $this->taskModel->filterTasks('id', $id);
+          $this->view->task = array_shift($filtered);
+      }
+   
+  }
 }
