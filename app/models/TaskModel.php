428ff85f183aae105d146062fd500f265c128fec
Dylan Goncalves <dylanphx93@gmail.com>
2026-06-17 22:18:27 +0200

    add task show and filter logic  to controller and model

--- app/models/TaskModel.php
+++ app/models/TaskModel.php
@@ -50,5 +50,39 @@ class TaskModel {
     file_put_contents($this->filePath, json_encode($tasks, JSON_PRETTY_PRINT));
   }
 
-  
+  public function filterTasks(string $field, string|int $value): array {
+      $tasks = $this->getAllTasks();
+      $newTasks = array_filter($tasks, function($task) use ($field, $value) {
+         switch ($field) {
+          case 'id':
+            return $task['id'] == $value;
+            
+          case 'title':
+            return $task['title'] == $value;
+            
+          case 'description':
+            return $task['description'] == $value;
+            
+          case 'status':
+            return $task['status'] == $value;
+            
+          case 'created_at':
+            return $task['created_at'] == $value;
+
+          case 'start_time':
+            return $task['start_time'] == $value;
+
+          case 'end_time':
+            return $task['end_time'] == $value;
+
+          case 'user':
+            return $task['user'] == $value;
+
+          default: 
+            return false;
+         }
+         
+     });
+     return array_values($newTasks);
+   }
 }
